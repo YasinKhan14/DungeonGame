335ca87730cc2951b40412374e4457e05522a665
Yasin <z5265047@ad.unsw.edu.au>
2020-07-18 16:24:46 +1000

    updated enemey, with implementation of nextMove()

--- src/unsw/dungeon/Enemy.java
+++ src/unsw/dungeon/Enemy.java
@@ -1,4 +1,7 @@
 package unsw.dungeon;
+import java.lang.Math;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.lang.model.util.ElementScanner6;
 
@@ -7,10 +10,57 @@ public class Enemy extends Entity implements Interactable, Moveable {
 	private boolean onMap;
 	private Dungeon dungeon;
 
+
     public Enemy(int x, int y) {
 		super(x, y);
 		onMap = true;
-    }
+	}
+	
+	public void greedyApproach(Player player) {
+		double leftCost = 0;
+		double rightCost = 0;
+		double upCost = 0;
+		double downCost = 0;
+
+		if (canMove(getX(), getY() + 1))
+			upCost = euclideanDistance(getX(), getY() + 1, player.getX(), player.getY());
+
+		if(canMove(getX(), getY() - 1))
+			downCost = euclideanDistance(getX(), getY() - 1, player.getX(), player.getY());
+
+		if(canMove(getX() -1, getY()))
+			leftCost = euclideanDistance(getX() - 1, getY(), player.getX(), player.getY());
+
+		if (canMove(getX() + 1, getY()))
+			rightCost = euclideanDistance(getX() + 1, getY(), player.getX(), player.getY());
+
+		if (isMin(rightCost, leftCost, downCost, upCost))
+			moveRight();
+		else if (isMin(leftCost, rightCost, downCost, upCost))
+			moveLeft();
+		else if (isMin(upCost, leftCost, downCost, rightCost))
+			moveUp();
+		else 
+			moveDown();
+
+		
+
+	}
+
+	public boolean isMin(double comp, double x1, double x2, double x3) {
+		double costArray[] = {comp, x1, x2, x3};
+		for (double d : costArray) {
+			if (comp > d)
+				return false;
+		}
+		return true;
+	}
+
+	public double euclideanDistance (int x1, int y1, int x2, int y2) {
+		double euclideanDistance = Math.sqrt((x2 - x1)*(x2 - x1) + (y2 - y1) * (y2 - y1));
+		return euclideanDistance;
+		
+	}
     
 	public int moveableIntersect(Moveable moveable) {
 		if (((Player) moveable).hasSword() > 0 ) 
@@ -25,32 +75,48 @@ public class Enemy extends Entity implements Interactable, Moveable {
 
 	@Override
 	public void moveUp() {
-		// TODO Auto-generated method stub
+		updateMap(getX(), getY() + 1);
 
 	}
 
 	@Override
 	public void moveDown() {
-		// TODO Auto-generated method stub
-
+		updateMap(getX(), getY() - 1);
 	}
 
 	@Override
 	public void moveLeft() {
-		// TODO Auto-generated method stub
+		updateMap(getX() - 1, getY());
 
 	}
 
 	@Override
 	public void moveRight() {
-		// TODO Auto-generated method stub
+		updateMap(getX() + 1, getY());
 
 	}
 
 	@Override
 	public boolean canMove(int x, int y) {
-		// TODO Auto-generated method stub
-		return false;
+		List<Entity> objectList = dungeon.getMap()[x][y];
+        for (Entity obj : objectList){
+            if (obj == null) {
+                continue;
+            }
+            // assuming wall is also interactable
+            if (!(obj instanceof Interactable)){
+                continue;
+            }
+            // type casting into interactables
+            Interactable i = (Interactable) obj;
+
+            switch(i.moveableIntersect(this)){
+                // wall or portal or door (clsoed) or boulder (for enemy)
+                case 1:
+                    return false;
+            }
+        }
+		return true;
 	}
 
 	@Override
