2885892f321f7b498764342abd1933e19825039d
Yasin <z5265047@ad.unsw.edu.au>
2020-07-20 16:35:47 +1000

    added import assertTrue for keytest

--- src/test/KeyTest.java
+++ src/test/KeyTest.java
@@ -1,7 +1,7 @@
 package test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import org.junit.jupiter.api.Test;
 
 import unsw.dungeon.*;
