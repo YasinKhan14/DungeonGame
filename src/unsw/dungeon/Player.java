d746036a8452689716a21555b79a29c1ffb48e15
Vincent Hu <z5155132@student.unsw.edu.au>
2020-07-25 21:21:57 +1000

    player dead will delete off map

--- src/unsw/dungeon/Player.java
+++ src/unsw/dungeon/Player.java
@@ -179,6 +179,7 @@ public class Player extends Entity implements Moveable{
 
     public void defeated() {
         this.setOffMap();
+        playerRemove(this);
         defeated.set(true);
     }
 
